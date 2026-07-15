512ebf5d8592c8b3da38cba8459cd2ace1867c6c
Bill <bill.li.luoshen@gmail.com>
2026-07-15 16:06:53 -0700

    added results

--- hoi4_conc_disp_comparison.R
+++ hoi4_conc_disp_comparison.R
@@ -1,9 +1,10 @@
 library(ggplot2)
 prod_eff_growth = 0
-prod_eff_cap_base = 1
+prod_eff_cap_base = 0.9
 factory_output = 4.5
-factory_output_base = 0.9
-industry_tech_level = 4
+factory_output_base = 1.78
+industry_tech_level = 5
+days = 500
 if(industry_tech_level == 0){
   industry_tech_year = 1934
 }else if(industry_tech_level == 1){
@@ -22,8 +23,8 @@ prod_eff_base_d = 0.1+0.05*industry_tech_level
 prod_eff_base_c = 0.1
 factory_output_c = factory_output_base+0.15*industry_tech_level
 factory_output_d = factory_output_base+0.1*industry_tech_level
-ic_c = vector("numeric", 500)
-ic_d = vector("numeric", 500)
+ic_c = vector("numeric", days)
+ic_d = vector("numeric", days)
 for(i in 1:500){
   ic_c[i] = factory_output*factory_output_c*prod_eff_base_c
   ic_d[i] = factory_output*factory_output_d*prod_eff_base_d
@@ -32,15 +33,15 @@ for(i in 1:500){
 }
 ic_cum_c = ic_c
 ic_cum_d = ic_d
-for(i in 2:500){
+for(i in 2:days){
   ic_cum_c[i] = ic_cum_c[i-1]+ic_c[i]
   ic_cum_d[i] = ic_cum_d[i-1]+ic_d[i]
 }
-plot_data = data.frame(day=rep(1:500,times=2),ic=c(ic_cum_c,ic_cum_d),type=rep(c('Concentrated','Dispersed'),each=500))
+plot_data = data.frame(day=rep(1:days,times=2),ic=c(ic_cum_c,ic_cum_d),type=rep(c('Concentrated','Dispersed'),each=days))
 intersection_label = min(which(ic_cum_c >= ic_cum_d))
 ggplot(plot_data) + geom_line(aes(x=day,y=ic,colour=type))+
   geom_label(aes(x=day,y=ic,label = format(round(ic,2),big.mark=",",scientific=FALSE)), 
-             data = . %>% filter(row_number() == 500),nudge_x=-10, nudge_y=100) + 
+             data = . %>% filter(row_number() == days),nudge_x=-10, nudge_y=100) + 
   labs(title = paste0(industry_tech_year, " level industry tech, ",
                       label_percent()(factory_output_c)," Concentrated factory output,\n",
                       label_percent()(prod_eff_cap)," Production Efficiency Cap, ",
