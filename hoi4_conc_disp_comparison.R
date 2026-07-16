465061c9ffa27eb82da7a14355c618e34cddc96f
Bill <bill.li.luoshen@gmail.com>
2026-07-16 09:52:14 -0700

    Added library requirements and retention calculation

--- hoi4_conc_disp_comparison.R
+++ hoi4_conc_disp_comparison.R
@@ -1,10 +1,19 @@
+library(dplyr)
+library(scales)
 library(ggplot2)
+## change modifiers here
 prod_eff_growth = 0
 prod_eff_cap_base = 0.9
 factory_output = 4.5
 factory_output_base = 1.78
-industry_tech_level = 5
-days = 500
+industry_tech_level = 4
+current_eff = 0.5
+## if you don't want to do retention calculation then set this to 0
+retention_base = 0.2
+## days of calculation
+days = 1000
+## days to plot in case the break-even point is too small
+plot_days = 1000
 if(industry_tech_level == 0){
   industry_tech_year = 1934
 }else if(industry_tech_level == 1){
@@ -19,13 +28,14 @@ if(industry_tech_level == 0){
   industry_tech_year = 1943
 }
 prod_eff_cap = prod_eff_cap_base+0.1*industry_tech_level
-prod_eff_base_d = 0.1+0.05*industry_tech_level
-prod_eff_base_c = 0.1
 factory_output_c = factory_output_base+0.15*industry_tech_level
 factory_output_d = factory_output_base+0.1*industry_tech_level
+factory_retention = 0.1*industry_tech_level
+prod_eff_base_d = current_eff * (retention_base+(1-retention_base)*factory_retention)
+prod_eff_base_c = current_eff * retention_base
 ic_c = vector("numeric", days)
 ic_d = vector("numeric", days)
-for(i in 1:500){
+for(i in 1:days){
   ic_c[i] = factory_output*factory_output_c*prod_eff_base_c
   ic_d[i] = factory_output*factory_output_d*prod_eff_base_d
   prod_eff_base_c = min(prod_eff_base_c+0.001*(prod_eff_cap)^2/prod_eff_base_c*(1+prod_eff_growth),prod_eff_cap)
@@ -37,11 +47,11 @@ for(i in 2:days){
   ic_cum_c[i] = ic_cum_c[i-1]+ic_c[i]
   ic_cum_d[i] = ic_cum_d[i-1]+ic_d[i]
 }
-plot_data = data.frame(day=rep(1:days,times=2),ic=c(ic_cum_c,ic_cum_d),type=rep(c('Concentrated','Dispersed'),each=days))
+plot_data = data.frame(day=rep(1:plot_days,times=2),ic=c(ic_cum_c[1:plot_days],ic_cum_d[1:plot_days]),type=rep(c('Concentrated','Dispersed'),each=plot_days))
 intersection_label = min(which(ic_cum_c >= ic_cum_d))
 ggplot(plot_data) + geom_line(aes(x=day,y=ic,colour=type))+
   geom_label(aes(x=day,y=ic,label = format(round(ic,2),big.mark=",",scientific=FALSE)), 
-             data = . %>% filter(row_number() == days),nudge_x=-10, nudge_y=100) + 
+             data = . %>% filter(row_number() == plot_days),nudge_x=-10, nudge_y=100) + 
   labs(title = paste0(industry_tech_year, " level industry tech, ",
                       label_percent()(factory_output_c)," Concentrated factory output,\n",
                       label_percent()(prod_eff_cap)," Production Efficiency Cap, ",
